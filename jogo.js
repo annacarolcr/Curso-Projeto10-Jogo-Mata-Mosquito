85ad208c2d2a8d7632b8d197e5a4f0f9d704840d
Anna Carolina Rocha <annacarolinarocha11@gmail.com>
2021-09-02 07:50:34 -0300

    controlando os pontos de vida

--- jogo.js
+++ jogo.js
@@ -1,6 +1,7 @@
 
 var altura = 0
 var largura = 0
+var vidas = 1
 
 function ajustaTamanhoPalcoJogo(){
     altura = window.innerHeight
@@ -15,6 +16,10 @@ function posicaoRandomica(){
     //remover o mosquito anterior (caso exista)
     if(document.getElementById('mosquito')){
         document.getElementById('mosquito').remove()
+
+        console.log('Elemento selecionado foi: v' + vidas)
+        document.getElementById('v' + vidas).src="imagens/coracao_vazio.png"
+        vidas++
     }
 
     var posicaoX = Math.floor(Math.random() * largura) - 90 //subtrai 90 para que a imagem do mosquito caiba na tela ao aparecer próximo ao limite da tela
@@ -33,6 +38,9 @@ function posicaoRandomica(){
     mosquito.style.top = posicaoY + 'px'
     mosquito.style.position = 'absolute'
     mosquito.id = 'mosquito'
+    mosquito.onclick = function(){
+        this.remove()
+    }
 
     document.body.appendChild(mosquito)
 }
