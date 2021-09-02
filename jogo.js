bc6addc1250cc86c1b0e4cf0290b559702dd9f0c
Anna Carolina Rocha <annacarolinarocha11@gmail.com>
2021-09-01 22:57:04 -0300

    implementando tamanhos randomicos nos mosquitos

--- jogo.js
+++ jogo.js
@@ -14,7 +14,7 @@ function posicaoRandomica(){
     var posicaoX = Math.floor(Math.random() * largura) - 90 //subtrai 90 para que a imagem do mosquito caiba na tela ao aparecer próximo ao limite da tela
     var posicaoY = Math.floor(Math.random() * altura) - 90
 
-    posicaoX = posicaoX < 0 ? 0 : posicaoX //resolve o problema de quando as posicoes x e y se coincidem com 0 - 90, valor negativo. se for menor que 0, ela passa a ser 0 ou recebe o valor de posicaoX
+    posicaoX = posicaoX < 0 ? 0 : posicaoX //resolve o problema de quando as posicoes x e y forem iguais a 0 - 90, ou seja, valor negativo (o mosquito some da tela). Se for menor que 0, ela passa a ser 0 ou recebe o valor de posicaoX.
     posicaoY = posicaoY < 0 ? 0 : posicaoY
 
     console.log(posicaoX, posicaoY)
@@ -22,11 +22,25 @@ function posicaoRandomica(){
     //Criar o elemento HTML
     var mosquito = document.createElement('img')
     mosquito.src = 'imagens/mosquito.png'
-    mosquito.className = 'mosquito1'
+    mosquito.className = tamanhoAleatorio()
     mosquito.style.left = posicaoX + 'px'
     mosquito.style.top = posicaoY + 'px'
     mosquito.style.position = 'absolute'
 
     document.body.appendChild(mosquito)
+}
+
+function tamanhoAleatorio(){
+    var classe = Math.floor(Math.random() * 3)
+    
+    switch(classe){
+        case 0:
+            return 'mosquito1'
+
+        case 1:
+            return 'mosquito2'
+
+        case 2:
+    }       return 'mosquito3'
 }
 
